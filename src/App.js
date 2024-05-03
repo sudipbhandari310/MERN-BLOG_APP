7148f8eed435a53054ffc9c319a59825187bcd3b
Sudip <sudipbhandari310@gmail.com>
2024-05-03 13:18:54 +0545

    user ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./styles/App.css";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <main>
+      <header className="header">
+        <a href="" className="logo">
+          Insorts
         </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://www.shutterstock.com/image-photo/patan-ancient-city-kathmandu-valley-260nw-551112193.jpg"
+            alt="image"
+          />
+        </div>
+        <div className="texts">
+          <h2>
+            Turkey halts trade with Israel over 'humanitarian tragedy' in Gaza
+          </h2>
+          <p className="info">
+            <a className="author">Sudip Bhandari</a>
+            <time>2024-1-2 45:23</time>
+          </p>
+          <p className="summary">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus,
+            voluptate provident ipsum quis iste animi nesciunt, obcaecati
+            dolorem aut deserunt explicabo quae minus magni, sit assumenda
+            necessitatibus! Voluptas, nesciunt! Autem!
+          </p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://www.shutterstock.com/image-photo/patan-ancient-city-kathmandu-valley-260nw-551112193.jpg"
+            alt="image"
+          />
+        </div>
+
+        <div className="texts">
+          <h2>
+            Turkey halts trade with Israel over 'humanitarian tragedy' in Gaza
+          </h2>
+
+          <p className="info">
+            <a className="author">Sudip Bhandari</a>
+            <time>2024-1-2 45:23</time>
+          </p>
+          <p className="summary">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus,
+            voluptate provident ipsum quis iste animi nesciunt, obcaecati
+            dolorem aut deserunt explicabo quae minus magni, sit assumenda
+            necessitatibus! Voluptas, nesciunt! Autem!
+          </p>
+        </div>
+      </div>
+    </main>
   );
-}
+};
 
 export default App;
